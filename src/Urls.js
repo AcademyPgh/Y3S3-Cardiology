fa159087265031d6f8813f0f3c7fc527d091ff2d
Thomas Scarlato <tscarlato@gmail.com>
2019-03-16 23:30:21 -0400

    Update Urls.js

--- src/Urls.js
+++ src/Urls.js
@@ -1,6 +1,6 @@
 export const Games = [
     {
-        name: "Playing Cards",
+        name: "Playing Cards (Spades mostly)",
         url: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/fc17a4ae-3af5-4257-a413-bedd4d97a937/image?iterationId=7442cf93-5551-4c61-b21b-916bb9dc954a",
         key: "054801a27f9a49519e0db20c8bcc5a5c",
     },
@@ -12,6 +12,16 @@ export const Games = [
     {
         name: "Coup",
         url: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/681ab4cd-45d8-4157-a2d5-b8dc8c65a532/image?iterationId=88813f94-5310-4afd-9768-a0619838a3da",
+        key: "054801a27f9a49519e0db20c8bcc5a5c",
+    },
+    {
+        name: "Fives and Sixes",
+        url: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/6bc485e1-4677-4bb5-8912-bd2fd2570589/image?iterationId=075353f9-1641-47ea-8661-0af9c4147b94",
+        key: "054801a27f9a49519e0db20c8bcc5a5c",
+    },
+    {
+        name: "Jixia Academy",
+        url: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/51c83bf7-6de8-46ab-80cb-1d6b18818077/image?iterationId=b75b90e3-cb65-4500-bb2f-493e60b8344c",
         key: "054801a27f9a49519e0db20c8bcc5a5c",
     },
     {
